0672a4aac8bb5d8cbb2a12d914e846a76476e858
Amie Wilt <amie.wilt@gmail.com>
2015-07-08 11:34:43 -0400

    added name to cart, added some cart views

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,12 +1,47 @@
 class CartsController < ApplicationController
 
+  def index
+    @carts = Cart.all
+    # quantity = Hash.new(0)
+    # total_price = 0
+    # current_user.cart.each do |i|
+    #   item = Item.find(i)
+    #   puts item.name
+    #   quantity[i] += 1
+    #   total_price += item.price_publix
+    # end
+    # puts quantity
+    # puts total_price
+    # c = Cart.new
+    # c.price_publix = total_price
+    # c.quantity = quantity
+    # c.name = "Cart"
+    # c.save
+    # @cart = Cart.find(c.id)
+    # puts @cart
+  end
+
   def show
     redirect_to(root_path) unless current_cart
     @cart = current_cart
   end
 
+  def new
+    @cart = Cart.new
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+
+    if @cart.save
+      redirect_to @cart, notice: 'Cart was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def update
-    product = Item.find(params[:item_id])
+    item = Item.find(params[:item_id])
     line = current_cart.line_items.find_or_create_by(:item => @item)
 
     if params[:quantity]
@@ -23,29 +58,20 @@ class CartsController < ApplicationController
     end
   end
 
-  def index
-    quantity = Hash.new(0)
-    total_price = 0
-    current_user.cart.each do |i|
-      item = Item.find(i)
-      puts item.name
-      quantity[i] += 1
-      total_price += item.price_publix
-    end
-    puts quantity
-    puts total_price
-    c = Cart.new
-    c.price_publix = total_price
-    c.quantity = quantity
-    c.name = "Cart"
-    c.save
-    @cart = Cart.find(c.id)
-    puts @cart
-  end
+
 
   
 
   private
+
+  def set_cart
+    @cart = current_user.carts.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def cart_params
+    params.require(:cart).permit(:user_id, :total)
+  end
 
   def current_cart
     @current_cart ||= (super || Cart.create!.tap { |c| session[:cart_id] = c.id })
